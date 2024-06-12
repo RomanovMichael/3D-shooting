b7305176c57725b81fd9e08ff656b6df828348b1
Romanov Michael <romanov.players@gmail.com>
2024-06-12 16:55:36 +0300

    feat: moving weapon animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
 /* eslint-disable */
-import {Sky, PointerLockControls} from '@react-three/drei'
-import {Ground} from './Ground.jsx'
-import {Player} from './Player.jsx'
-import {Physics} from '@react-three/rapier'
-import { Cubes } from './Cubes.jsx'
-import { WeaponModel } from './WeaponModel.jsx'
+import * as TWEEN from "@tweenjs/tween.js";
+import {PointerLockControls, Sky} from "@react-three/drei";
+import {Ground} from "./Ground.jsx";
+import {Physics} from "@react-three/rapier";
+import {Player} from "./Player.jsx";
+import {Cubes} from "./Cubes.jsx";
+import {useFrame} from "@react-three/fiber";
 
-const shadowOffset = 50
+const shadowOffset = 50;
 
 export const App = () => {
+    useFrame(() => {
+        TWEEN.update();
+    });
 
-  return (
-    <>
-      <PointerLockControls />
-      <Sky sunPosition={[100, 20, 100]} />
-      <ambientLight intensity={1.5} />
-      <directionalLight
-        castShadow
-        intensity={1.5}
-        shadow-mapSize={4096}
-        shadow-camera-top={shadowOffset}
-        shadow-camera-bottom={-shadowOffset}
-        shadow-camera-left={shadowOffset}
-        shadow-camera-right={-shadowOffset}
-        position={[100, 100, 0]} 
-        />
-      <Physics gravity={[0, -20, 0]}>
-        <Ground />
-        <Player />
-        <Cubes />
-      </Physics>
-      <group position={[3, 1, -2]}>
-        <WeaponModel/>
-      </group>
-    </>
-  )
+    return (
+        <>
+            <PointerLockControls />
+            <Sky sunPosition={[100, 20, 100]}/>
+            <ambientLight intensity={1.5} />
+            <directionalLight
+                castShadow
+                intensity={1.5}
+                shadow-mapSize={4096}
+                shadow-camera-top={shadowOffset}
+                shadow-camera-bottom={-shadowOffset}
+                shadow-camera-left={shadowOffset}
+                shadow-camera-right={-shadowOffset}
+                position={[100, 100, 0]}
+            />
+            <Physics gravity={[0, -20, 0]}>
+                <Ground />
+                <Player />
+                <Cubes />
+            </Physics>
+        </>
+    )
 }
 
 export default App
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,67 +1,120 @@
 /* eslint-disable */
-import * as THREE from 'three'
+import * as THREE from "three";
 import * as RAPIER from "@dimforge/rapier3d-compat"
-import {CapsuleCollider, RigidBody, useRapier} from '@react-three/rapier'
-import { useRef } from 'react'
-import { usePersonControls } from './hooks'
-import { useFrame } from '@react-three/fiber'
+import * as TWEEN from "@tweenjs/tween.js";
+import {CapsuleCollider, RigidBody, useRapier} from "@react-three/rapier";
+import {useEffect, useRef, useState} from "react";
+import {usePersonControls} from "./hooks.js";
+import {useFrame} from "@react-three/fiber";
 import {Weapon} from "./Weapon.jsx";
 
-const MOVE_SPEED = 5
-const direction = new THREE.Vector3()
-const frontVector = new THREE.Vector3()
-const sideVector = new THREE.Vector3()
+const MOVE_SPEED = 5;
+const direction = new THREE.Vector3();
+const frontVector = new THREE.Vector3();
+const sideVector = new THREE.Vector3();
 const rotation = new THREE.Vector3();
 
 export const Player = () => {
-  const playerRef = useRef()
-  const { forward, backward, left, right, jump } = usePersonControls()
-  const objectInHandRef = useRef();
+    const playerRef = useRef();
+    const { forward, backward, left, right, jump } = usePersonControls();
+    const objectInHandRef = useRef();
 
-  const rapier = useRapier()
+    const swayingObjectRef = useRef();
+    const [swayingAnimation, setSwayingAnimation] = useState(null);
+    const [swayingBackAnimation, setSwayingBackAnimation] = useState(null);
+    const [isSwayingAnimationFinished, setIsSwayingAnimationFinished] = useState(true);
 
-  useFrame((state) => {
-    if (!playerRef.current) return;
-    // передвижение игрока
-    const velocity = playerRef.current.linvel();
-    frontVector.set(0, 0, backward - forward);
-    sideVector.set(left - right, 0, 0);
-    direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(MOVE_SPEED).applyEuler(state.camera.rotation)
+    const rapier = useRapier();
 
-    playerRef.current.wakeUp();
-    playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z })
+    useFrame((state) => {
+        if (!playerRef.current) return;
 
-    // прыжки
-    const world = rapier.world
-    const ray = world.castRay(new RAPIER.Ray(playerRef.current.translation(), { x: 0, y: -1, z: 0 }))
-    const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.5
+        // moving player
+        const velocity = playerRef.current.linvel();
 
-    if (jump && grounded) doJump()
+        frontVector.set(0, 0, backward - forward);
+        sideVector.set(left - right, 0, 0);
+        direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(MOVE_SPEED).applyEuler(state.camera.rotation);
 
-    // движение камеры
-    const { x, y, z } = playerRef.current.translation()
-    state.camera.position.set(x, y, z)
+        playerRef.current.wakeUp();
+        playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
 
-    // движение объекта в руке игрока
-    objectInHandRef.current.rotation.copy(state.camera.rotation);
-    objectInHandRef.current.position.copy(state.camera.position).add(state.camera.getWorldDirection(rotation));
-  })
+        // jumping
+        const world = rapier.world;
+        const ray = world.castRay(new RAPIER.Ray(playerRef.current.translation(), { x: 0, y: -1, z: 0 }));
+        const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.5;
 
-  const doJump = () => {
-    playerRef.current.setLinvel({x: 0, y: 8, z: 0})
-  }
+        if (jump && grounded) doJump();
 
-  return (
-    <>
-      <RigidBody position={[0, 1, -2]} mass={1} ref={playerRef} lockRotations>
-        <mesh castShadow>
-          <capsuleGeometry args={[0.5, 0.5]} />
-          <CapsuleCollider args={[0.75, 0.5]} />
-        </mesh>
-      </RigidBody>
-      <group ref={objectInHandRef}>
-        <Weapon position={[0.3, -0.1, 0.3]} scale={0.3} />
-      </group>
-    </>
-  )
+        // moving camera
+        const {x, y, z} = playerRef.current.translation();
+        state.camera.position.set(x, y, z);
+
+        // moving object in hand for the player
+        objectInHandRef.current.rotation.copy(state.camera.rotation);
+        objectInHandRef.current.position.copy(state.camera.position).add(state.camera.getWorldDirection(rotation));
+
+        const isMoving = direction.length() > 0;
+
+        if (isMoving && isSwayingAnimationFinished) {
+            setIsSwayingAnimationFinished(false);
+            swayingAnimation.start();
+        }
+
+        TWEEN.update();
+    });
+
+    const doJump = () => {
+        playerRef.current.setLinvel({x: 0, y: 8, z: 0});
+    }
+
+    const initSwayingObjectAnimation = () => {
+        const currentPosition = new THREE.Vector3(0, 0, 0);
+        const initialPosition = new THREE.Vector3(0, 0, 0);
+        const newPosition = new THREE.Vector3(-0.05, 0, 0);
+        const animationDuration = 300;
+        const easing = TWEEN.Easing.Quadratic.Out;
+
+        const twSwayingAnimation = new TWEEN.Tween(currentPosition)
+            .to(newPosition, animationDuration)
+            .easing(easing)
+            .onUpdate(() => {
+                swayingObjectRef.current.position.copy(currentPosition);
+            });
+
+        const twSwayingBackAnimation = new TWEEN.Tween(currentPosition)
+            .to(initialPosition, animationDuration)
+            .easing(easing)
+            .onUpdate(() => {
+                swayingObjectRef.current.position.copy(currentPosition);
+            })
+            .onComplete(() => {
+                setIsSwayingAnimationFinished(true);
+            });
+
+        twSwayingAnimation.chain(twSwayingBackAnimation);
+
+        setSwayingAnimation(twSwayingAnimation);
+        setSwayingBackAnimation(twSwayingBackAnimation);
+    }
+
+    useEffect(() => {
+        initSwayingObjectAnimation();
+    }, []);
+
+    return (
+        <>
+            <RigidBody colliders={false} mass={1} ref={playerRef} lockRotations>
+                <mesh castShadow>
+                    <capsuleGeometry args={[0.5, 0.5]}/>
+                    <CapsuleCollider args={[0.75, 0.5]} />
+                </mesh>
+            </RigidBody>
+            <group ref={objectInHandRef}>
+                <group ref={swayingObjectRef}>
+                    <Weapon position={[0.3, -0.1, 0.3]} scale={0.3} />
+                </group>
+            </group>
+        </>
+    );
 }
