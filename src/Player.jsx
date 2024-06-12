53b0529f7cc160e972d7f3e196da4ff606c21305
Romanov Michael <romanov.players@gmail.com>
2024-06-12 12:25:46 +0300

    feat(player): add jumping

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import * as THREE from 'three'
-import {RigidBody} from '@react-three/rapier'
+import * as RAPIER from "@dimforge/rapier3d-compat"
+import {RigidBody, useRapier} from '@react-three/rapier'
 import { useRef } from 'react'
 import { usePersonControls } from './hooks'
 import { useFrame } from '@react-three/fiber'
@@ -13,10 +14,13 @@ const sideVector = new THREE.Vector3()
 export const Player = () => {
   const playerRef = useRef()
   const { forward, backward, left, right, jump } = usePersonControls()
+  
+  const rapier = useRapier()
 
   useFrame((state) => {
       if (!playerRef.current) return;
 
+      // передвижение игрока
       const velocity = playerRef.current.linvel();
 
       frontVector.set(0, 0, backward - forward);
@@ -24,12 +28,23 @@ export const Player = () => {
       direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(MOVE_SPEED);
 
       playerRef.current.wakeUp();
-      playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
+      playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z })
+
+      // прыжки
+      const world = rapier.world
+      const ray = world.castRay(new RAPIER.Ray(playerRef.current.translation(), { x: 0, y: -1, z: 0 }))
+      const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1
+
+      if (jump && grounded) doJump()
   })
+
+  const doJump = () => {
+    playerRef.current.setLinvel({x: 0, y: 8, z: 0})
+  }
 
   return (
     <>
-      <RigidBody position={[0, 1, -2]} ref={playerRef}>
+      <RigidBody position={[0, 1, -2]} mass={1} ref={playerRef} lockRotations>
         <mesh>
           <capsuleGeometry args={[0.5, 0.5]} />
         </mesh>
